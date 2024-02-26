efc5a20c623d86d45f9ce7f783b8e4acc0e0047f
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-02-26 15:28:31 +0200

    I did all the ex from 8 to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestYear=0;
+    let oldest={};
 
+    people.forEach(person => {
+        if('yearOfDeath' in person){
+            let years=+person['yearOfDeath'] - +person['yearOfBirth'];
+            if(years>oldestYear || oldestYear===0){
+                oldestYear=years;
+                oldest=person;
+            }
+        }else{ 
+            let now=new Date().getFullYear() ;
+            let years_d= +now - +person['yearOfBirth'];
+            if(years_d>oldestYear || oldestYear===0){
+                oldestYear=years_d;
+                oldest=person;
+            }
+        }
+        
+    });
+    return oldest;
 };
 
 // Do not edit below this line
